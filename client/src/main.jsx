c5f07880f83cf9520c9ff7d089b5aaefb668156f
Brandon Wing <brandon.wing245@gmail.com>
2024-02-20 11:49:59 -0700

    Added header, navbar, and started home content.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import "./index.css";
-
 import App from "./App.jsx";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,18 @@
+import homeVideo from "../assets/images/dota-test-video.mp4";
+
 export default function Home() {
   return (
-    <div className="bg">
-      <div className="container">
+    <div className="video-container">
+      <div className="header">
         <h1>Test Home</h1>
+      </div>
+      <div className="video-div">
+        <video autoPlay muted playsInline loop className="home-vid">
+          <source type="video/mp4" src={homeVideo} />
+        </video>
+      </div>
+      <div className="video-text">
+        <h2>Hello There!</h2>
       </div>
     </div>
   );
