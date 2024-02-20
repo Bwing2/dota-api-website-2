00a5269f39ecc0fc577d977566c1a8c076c6b4ed
Brandon Wing <brandon.wing245@gmail.com>
2024-02-20 15:15:05 -0700

    Added react-intersection-observer and changed css for homepage.

--- client/src/components/HomePage/VideoWithText.jsx
+++ client/src/components/HomePage/VideoWithText.jsx
@@ -1,22 +1,20 @@
+import ScrollText from "../HomePage/ScrollText";
+
 import homeVideo from "../../assets/images/dota-test-video.mp4";
 import heroImage from "../../assets/images/dota-hero-image.jpg";
 
 export default function VideoWithText() {
   return (
-    <div className="video-container">
-      <div className="video-div">
+    <div className="container">
+      <div className="video-and-image-div">
         <video autoPlay muted playsInline loop className="home-vid">
           <source type="video/mp4" src={homeVideo} />
         </video>
-        <img src={heroImage} alt="hero image" className="hero-image" />
-      </div>
-      <div className="video-text-div">
-        <div className="home-title">Dota 2 Stats Tracker</div>
-        <div className="home-description">
-          Track game stats, player stats, and more!
+        <div className="image-div">
+          <img src={heroImage} alt="hero image" className="hero-image" />
         </div>
-        <button className="modal-button">Search By Player</button>
       </div>
+      <ScrollText />
     </div>
   );
 }
